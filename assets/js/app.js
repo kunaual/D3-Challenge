4b2e44e9a649a226893d40061ddad0ed6eaecf95
kunaual <aekunau@gmail.com>
2021-04-24 13:39:33 -0500

    pulled csv data into app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,162 @@
 // @TODO: YOUR CODE HERE!
+console.log("app.js loaded");
+
+// You need to create a scatter plot between two of the data variables such as Healthcare vs. Poverty or Smokers vs. Age.
+
+// Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. 
+// You'll code this graphic in the app.js file of your homework directory—make sure you pull in the data from data.csv by using the d3.csv function.
+// Your scatter plot should ultimately appear like the image at the top of this section.
+
+// Include state abbreviations in the circles.
+// Create and situate your axes and labels to the left and bottom of the chart.
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var chosenXAxis = "poverty";
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Retrieve data from the CSV file and execute everything
+  //files path starts from where index.html resides
+d3.csv("assets/data/data.csv").then(function(stateData, err) {
+    if (err) throw err;
+  console.log(stateData);
+    // converts things to numbers
+    stateData.forEach(function(data) {
+      data.hair_length = +data.hair_length;
+      data.num_hits = +data.num_hits;
+      data.num_albums = +data.num_albums;
+    });
+  
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(stateData, chosenXAxis);
+  
+    // Create y scale function
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(stateData, d => d.num_hits)])
+      .range([height, 0]);
+  
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+  
+    // append x axis
+    var xAxis = chartGroup.append("g")
+      .classed("x-axis", true)
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+  
+    // append y axis
+    chartGroup.append("g")
+      .call(leftAxis);
+  
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(stateData)
+      .enter()
+      .append("circle")
+      .attr("cx", d => xLinearScale(d[chosenXAxis]))
+      .attr("cy", d => yLinearScale(d.num_hits))
+      .attr("r", 20)
+      .attr("fill", "pink")
+      .attr("opacity", ".5");
+  
+    // Create group for two x-axis labels
+    var labelsGroup = chartGroup.append("g")
+      .attr("transform", `translate(${width / 2}, ${height + 20})`);
+  
+    var hairLengthLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .attr("value", "hair_length") // value to grab for event listener
+      .classed("active", true)
+      .text("Hair Metal Ban Hair Length (inches)");
+  //homework 16 something in here
+    var albumsLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 40)
+      .attr("value", "num_albums") // value to grab for event listener
+      .classed("inactive", true)   ///this maybe?
+      .text("# of Albums Released");
+  
+    // append y axis
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .classed("axis-text", true)
+      .text("Number of Billboard 500 Hits");
+  
+    // updateToolTip function above csv import
+    var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+  
+    // x axis labels event listener
+    labelsGroup.selectAll("text")
+      .on("click", function() {
+        // get value of selection
+        var value = d3.select(this).attr("value");
+        if (value !== chosenXAxis) {
+  
+          // replaces chosenXAxis with value
+          chosenXAxis = value;
+  
+          // console.log(chosenXAxis)
+  
+          // functions here found above csv import
+          // updates x scale for new data
+          xLinearScale = xScale(stateData, chosenXAxis);
+  
+          // updates x axis with transition
+          xAxis = renderAxes(xLinearScale, xAxis);
+  
+          // updates circles with new x values
+          circlesGroup = renderCircles(circlesGroup, xLinearScale, chosenXAxis);
+  
+          // updates tooltips with new info
+          circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+  
+          // changes classes to change bold text
+          if (chosenXAxis === "num_albums") {
+            albumsLabel
+              .classed("active", true)
+              .classed("inactive", false);
+            hairLengthLabel
+              .classed("active", false)
+              .classed("inactive", true);
+          }
+          else {
+            albumsLabel
+              .classed("active", false)
+              .classed("inactive", true);
+            hairLengthLabel
+              .classed("active", true)
+              .classed("inactive", false);
+          }
+        }
+      });
+  }).catch(function(error) {
+    console.log("File not read. error:")
+    console.log(error);
+  });
