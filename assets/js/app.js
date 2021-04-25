078e029f30b61c45823bac2a4611d1978b75ba08
kunaual <aekunau@gmail.com>
2021-04-25 09:57:42 -0500

    moving axis scaling to functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,25 +21,61 @@ var svg = d3.select("#scatter")
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+
+//init axis
+var selectedXAxis = "poverty";
+var selectedYAxis = "healthcare";
+
+// functions used for updating axis upon click on axis label
+function xScale(stateData, chosenXAxis) {
+
+  console.log("in xscale function");
+  console.log(stateData[0][chosenXAxis])
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenXAxis]) - 1,
+    d3.max(stateData, d => d[chosenXAxis]) + 1
+    ])
+    .range([0, width]);
+
+  return xLinearScale;
+
+}
+function yScale(stateData, chosenYAxis) {
+
+  console.log("in yscale function");
+  console.log(stateData[0][chosenYAxis])
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(stateData, d => d[chosenYAxis]) - 1, d3.max(stateData, d => d[chosenYAxis]) + 1])
+    .range([height, 0]);
+
+  return yLinearScale;
+
+}
+
+
 // Import Data
 d3.csv("assets/data/data.csv").then(function (stateData) {
-  console.log(stateData);
+  console.log(stateData); //testing output
+  //xScale(stateData,selectedXAxis);
 
   // convert strings to numbers
   stateData.forEach(function (data) {
     data.poverty = +data.poverty;
     data.healthcare = +data.healthcare;
+    data.income = +data.income;
   });
   console.log(stateData[0].state + " " + stateData[0].abbr);
-  // Step 2: Create scale functions
-  // ==============================
-  var xLinearScale = d3.scaleLinear()
-    .domain([d3.min(stateData, d => d.poverty) - 1, d3.max(stateData, d => d.poverty)])
-    .range([0, width]);
 
-  var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(stateData, d => d.healthcare)])
-    .range([height, 0]);
+  // ==============================
+  var xLinearScale = xScale(stateData, selectedXAxis);
+  // var xLinearScale = d3.scaleLinear()
+  //   .domain([d3.min(stateData, d => d.poverty) - 1, d3.max(stateData, d => d.poverty)])
+  //   .range([0, width]);
+
+  var yLinearScale = yScale(stateData, selectedYAxis);
+  // var yLinearScale = d3.scaleLinear()
+  //   .domain([0, d3.max(stateData, d => d.healthcare)])
+  //   .range([height, 0]);
 
   // define axes
   var bottomAxis = d3.axisBottom(xLinearScale);
@@ -54,7 +90,7 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
     .call(leftAxis);
 
 
-  // ==============================
+  // build tooltip for hoverover text display
   var toolTip = d3.tip()
     .attr("class", "d3-tip")
     .offset([80, -60])
@@ -63,8 +99,7 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
       return (`${d.state}<br>Poverty: ${d.poverty}%<br>Lacking Healthcare: ${d.healthcare}%`);
     });
 
-  // Step 5: Create Circles
-  // ==============================
+  // Create Circles
   var circlesGroup = chartGroup.selectAll("circle")
     .data(stateData)
     .enter()
@@ -79,8 +114,8 @@ d3.csv("assets/data/data.csv").then(function (stateData) {
     .on('mouseout', toolTip.hide);
 
   circlesGroup.append("text")
-  //.attr("dx", function(d){return -5})
-  .text(function(d){return d.abbr})
+    .attr("dx", function (d) { return -5 })
+    .text(function (d) { return d.abbr })
 
   // put tooltips in the chart
   chartGroup.call(toolTip);
